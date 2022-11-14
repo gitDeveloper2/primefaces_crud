e010226e9a94e3aa279d68b737e3ac80ec6f0e8c
horace karatu <horace.karatu@gmail.com>
2022-11-14 10:54:16 +0300

    errors

--- src/java/controllers/PersonController.java
+++ src/java/controllers/PersonController.java
@@ -1,10 +1,13 @@
 
 package controllers;
 import java.io.Serializable;
+import java.sql.SQLException;
 import models.Person;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 
@@ -32,13 +35,17 @@ public class PersonController implements Serializable{
     private PersonService personService;
   private Map<String,Object> sessionMap=FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
     @PostConstruct
-    public void init(){
-//        persons=new ArrayList<>();
+    public void init() {
+        try {
+            //        persons=new ArrayList<>();
 //        persons.add(new Person(1, "Angela"));
 //        persons.add(new Person(2, "Milo"));
-        
- 
-         persons=personService.getPersons();
+
+
+persons=personService.getPersons();
+        } catch (SQLException ex) {
+            Logger.getLogger(PersonController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     public PersonController() {
     }
@@ -88,22 +95,23 @@ public class PersonController implements Serializable{
         this.name = name;
     }
     
-    public String create(){
+    public String create() throws SQLException{
         Person p=new Person();
         p.setName(name);
         personService.create(p);
         return "index?faces-redirect=true";
     }
-    public  String delete(){
+    public  String delete() throws SQLException{
         Map<String,String> params=FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
         int id=Integer.parseInt(params.get("delete"));
         personService.delete(id);
+        System.out.println("doene");
         return "index?faces-redirect=true";
     }
-    public String update(Person p){
-        System.out.println("Avail"+p);
-    Map<String,String> params=FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-    String id=params.get("update");
+    public String update(Person p) throws SQLException{
+        System.out.println("Available"+p);
+//    Map<String,String> params=FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+//    String id=params.get("update");
    // Person p=new Person();
 //    p.setId(Integer.parseInt(id));
 //    p.setName(name);
@@ -111,15 +119,15 @@ public class PersonController implements Serializable{
     personService.update(p);
      return "index?faces-redirect=true";
     }
-       public  String edit(){
-           
+       public  String edit() throws SQLException{
+            System.out.println("methodss");
            FacesContext fc=FacesContext.getCurrentInstance();
            Map<String,String> params=fc.getExternalContext().getRequestParameterMap();
            int id=Integer.parseInt(params.get("edit1"));
           Person p=personService.get(id);
-           System.out.println(p);
+          
            sessionMap.put("editedperson", p);
-        return "/edit.xhtml?faces-redirect=true";
+        return "/index.xhtml?faces-redirect=true";
     }
 
 
